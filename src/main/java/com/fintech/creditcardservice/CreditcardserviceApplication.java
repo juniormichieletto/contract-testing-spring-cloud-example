06ac87b577c20321e0d021a352aa1aaa00d6bd27
Aroldo D Miqueletti Junior <aroldo.junior@db1.com.br>
2019-06-15 16:56:44 -0300

    First commit

---
+++ src/main/java/com/fintech/creditcardservice/CreditcardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.creditcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintech/creditcardservice/CreditcardserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fintech.creditcardservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditcardserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
