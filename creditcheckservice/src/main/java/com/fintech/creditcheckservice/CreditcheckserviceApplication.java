ce60b48369275c2fc82d077aa942672832d66e0c
Aroldo D Miqueletti Junior <aroldo.junior@db1.com.br>
2019-06-16 09:18:13 -0300

    Add creditCheckService with first contract

---
+++ creditcheckservice/src/main/java/com/fintech/creditcheckservice/CreditcheckserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.creditcheckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcheckserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcheckserviceApplication.class, args);
+	}
+
+}
