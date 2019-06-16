d1b084def6275876a39ea0b2ee1e6ff4749c2e68
Aroldo D Miqueletti Junior <aroldo.junior@db1.com.br>
2019-06-16 09:52:14 -0300

    Add StubVerifier to read contract

--- creditcardservice/src/test/java/com/fintech/creditcardservice/CreditcardserviceApplicationTests.java
+++ creditcardservice/src/test/java/com/fintech/creditcardservice/CreditcardserviceApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,6 +19,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureStubRunner(
+    ids = "com.fintech:creditcheckservice:+:stubs:8080",
+    stubsMode = StubRunnerProperties.StubsMode.LOCAL
+)
 public class CreditcardserviceApplicationTests {
 
 	@Autowired
