5467b9561c451119322a0c1a7c66923766a69f8a
Aroldo D Miqueletti Junior <aroldo.junior@db1.com.br>
2019-06-16 10:01:44 -0300

    Adding baseUrl to get by property on CreditCardApplicationsController

--- creditcardservice/src/main/java/com/fintech/creditcardservice/controller/CreditCardApplicationsController.java
+++ creditcardservice/src/main/java/com/fintech/creditcardservice/controller/CreditCardApplicationsController.java
@@ -3,19 +3,26 @@ package com.fintech.creditcardservice.controller;
 import com.fintech.creditcardservice.domain.CardType;
 import com.fintech.creditcardservice.domain.CreditCardStatus;
 import com.fintech.creditcardservice.domain.Score;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class CreditCardApplicationsController {
 
     private final RestTemplate restTemplate;
+    private final String creditCheckServiceBaseUrl;
 
-    public CreditCardApplicationsController(RestTemplate restTemplate) {
+    public CreditCardApplicationsController(
+        RestTemplate restTemplate,
+        @Value("${creditcheckservice.baseurl}") String creditCheckServiceBaseUrl
+    ) {
         this.restTemplate = restTemplate;
+        this.creditCheckServiceBaseUrl = creditCheckServiceBaseUrl;
     }
 
     @PostMapping(
@@ -26,14 +33,20 @@ public class CreditCardApplicationsController {
             @RequestBody final ApplyForCreditCardRequest applyForCreditCardRequest
     ) {
         final Integer citizenNumber = applyForCreditCardRequest.getCitizenNumber();
+
+        final String url = UriComponentsBuilder.fromHttpUrl(creditCheckServiceBaseUrl)
+            .path("credit-scores")
+            .toUriString();
+
         final CreditCheckResponse creditCheckResponse = restTemplate
-                .postForObject(
-                        "http://localhost:8080/credit-scores",
-                        new CreditCheckRequest(citizenNumber),
-                        CreditCheckResponse.class);
+            .postForObject(
+                url,
+                new CreditCheckRequest(citizenNumber),
+                CreditCheckResponse.class);
 
         if (Score.HIGH == creditCheckResponse.getScore() &&
                 CardType.GOLD == applyForCreditCardRequest.getCardType()) {
+
             return new ApplyForCreditCardResponse(CreditCardStatus.GRANTED);
         }
         throw new RuntimeException("Card and score not yet implemented");
